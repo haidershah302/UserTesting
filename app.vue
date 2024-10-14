cf48db86c03bc3e7c5c85f4062eb2694fd488756
Haider Shah <stfox302@gmail.com>
2024-10-15 01:18:53 +0500

    custome error

--- app.vue
+++ app.vue
@@ -3,7 +3,32 @@
       <NuxtRouteAnnouncer />
       <NuxtLoadingIndicator />
       <NuxtLayout>
-          <NuxtPage />
+<!--          <NuxtPage />-->
+          <div role="alert" class="alert alert-error">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24">
+                  <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span>Unable to fetch data from api https://FLy-live.org</span>
+          </div>
       </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+
+try {
+    throw 'Unable to fetch data from https://www.fly-live.org'
+    // Some API request
+} catch (err) {
+    console.log(err)
+}
+
+</script>
