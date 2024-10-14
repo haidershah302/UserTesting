794173edea6403ef31862fe4049c5d324570166e
Haider Shah <stfox302@gmail.com>
2024-10-15 01:08:43 +0500

    001> create Profile Page

--- layouts/profileLayout.vue
+++ layouts/profileLayout.vue
@@ -17,10 +17,9 @@
                     viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M4 6h16M4 12h16M4 18h7" />
+                        stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" d="M4 6h16M4 12h16M4 18h7"
+                    />
                 </svg>
             </NuxtLink>
         </div>
